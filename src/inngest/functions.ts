ba0df4f574d51aff7b8bfa57a5d645b87fdf9b3c
satyam-code45 <satyam45.dev@gmail.com>
2025-06-29 10:47:06 +0530

    e2b sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,14 +1,16 @@
 import { inngest } from "./client";
-import {
-  Agent,
-  openai,
-  createAgent,
-} from "@inngest/agent-kit";
+import { openai, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("blitz");
+      return sandbox.sandboxId;
+    });
     const summarizer = createAgent({
       name: "summarizer",
       system: "You are an expert summarizer.  You summarize in 10 words.",
@@ -18,7 +20,13 @@ export const helloWorld = inngest.createFunction(
     const { output } = await summarizer.run(
       `summarize the following text ${event.data.value}`
     );
-    console.log(output);
-    return { output };
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
