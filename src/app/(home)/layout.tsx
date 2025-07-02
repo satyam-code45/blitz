a8fe39f5fd4d5e1571b8d550b2593319eeb6f5bb
satyam-code45 <satyam45.dev@gmail.com>
2025-07-02 10:56:48 +0530

    home page update

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -5,12 +5,16 @@ interface Props {
 const Layout = ({ children }: Props) => {
   return (
     <main className="flex flex-col min-h-screen max-h-screen">
-      <div
-        className="absolute inset-0 -z-10 h-full w-full bg-background 
-        dark:bg-[radial-gradient(#ffffff20_1px,transparent_1px)] 
-        bg-[radial-gradient(#dadde2_1px,transparent_1px)] 
-        [background-size:16px_16px]"
-      />
+<div
+  className="absolute inset-0 -z-10 h-full w-full 
+    bg-background 
+    dark:bg-[radial-gradient(#ffffff20_1px,transparent_1px)] 
+    bg-[radial-gradient(#dadde2_1px,transparent_1px)] 
+    [background-size:16px_16px] 
+    [mask-image:linear-gradient(to_bottom,black_0%,black_50%,transparent_100%)] 
+    [mask-mode:match-source]"
+/>
+
 
       <div className="flex-1 flex flex-col px-4 pb-4">{children}</div>
     </main>
